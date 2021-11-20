831513e6447b8ed41c9a214e6d392fd549447d66
Varun Prakash <varun.prak@gmail.com>
2021-11-20 16:49:24 +0800

    Updated models.py

--- ecomsite/models.py
+++ ecomsite/models.py
@@ -12,7 +12,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7,decimal_places=2)
     image = models.ImageField(null=True,blank=True)
     digital = models.BooleanField(default=False)
 
