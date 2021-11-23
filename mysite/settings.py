05c6a838afb8f3106d42f66fb0ada68a82729ac9
varunbkk <varun.prak@gmail.com>
2021-11-23 11:10:53 +0800

    Updated project settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = BASE_DIR/'templates'
+TEMPLATE_DIR = BASE_DIR/'ecomsite/templates/ecomsite'
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,5 +130,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 MEDIA_URL = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
