5ebb1cec43169363a9f8af1423459a0124bc81e5
varunbkk <varun.prak@gmail.com>
2021-11-23 11:11:24 +0800

    Updated project urls with Login and Logout Views

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,13 @@ from django.urls import path,include
 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('',include('ecomsite.urls')),
+    path('accounts/login/',auth_views.LoginView.as_view(),name='login'),
+    path('accounts/logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('admin/', admin.site.urls),
 ]
 
